b2e8c4d635c8d602ccdf2f4b5f3253b0fa7fb78e
Rubén Pruñanosa <rubenprunyanosa@gmail.com>
2019-04-04 21:05:06 +0200

    + Ahora se borra antes de repopular.

--- tutorialExpressMongoDB/populatedb.js
+++ tutorialExpressMongoDB/populatedb.js
@@ -1,5 +1,4 @@
 #! /usr/bin/env node
-
 console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true');
 
 // Get arguments passed on command line
@@ -17,13 +16,29 @@ var Genre = require('./models/genre')
 var BookInstance = require('./models/bookinstance')
 var Country = require('./models/countries')
 
-
 var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Eliminar colecciones antes del populate
+db.collections['countries'].drop(function(err){
+  console.log('Se ha borrado la patata de datos....');
+});
+db.collections['authors'].drop(function(err){
+  console.log('Se ha borrado la patata de datos....');
+});
+db.collections['books'].drop(function(err){
+  console.log('Se ha borrado la patata de datos....');
+});
+db.collections['bookinstances'].drop(function(err){
+  console.log('Se ha borrado la patata de datos....');
+});
+db.collections['genres'].drop(function(err){
+  console.log('Se ha borrado la patata de datos....');
+});
 
 var authors = []
 var genres = []
