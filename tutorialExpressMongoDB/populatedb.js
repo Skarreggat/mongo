94d893e265a5c3bc87401faea273f63bad07d895
timlest <43617154+timlest@users.noreply.github.com>
2019-04-10 16:43:19 +0200

    Formato casi terminado a la espera de comprovar errores...

--- tutorialExpressMongoDB/populatedb.js
+++ tutorialExpressMongoDB/populatedb.js
@@ -15,6 +15,7 @@ var Author = require('./models/author')
 var Genre = require('./models/genre')
 var BookInstance = require('./models/bookinstance')
 var Country = require('./models/countries')
+var Formato = require('./models/formato')
 
 var mongoose = require('mongoose');
 var mongoDB = userArgs[0];
@@ -35,6 +36,8 @@ db.collections['bookinstances'].drop(function(err){
 });
 db.collections['genres'].drop(function(err){
 });
+db.collections['formatos'].drop(function(err){//formatos o formato?
+});
 console.log('Se han eliminado todos los registros de la base de datos.');
 
 var authors = []
@@ -42,6 +45,7 @@ var genres = []
 var books = []
 var bookinstances = []
 var countries = []
+var formatos = []
 
 function authorCreate(first_name, family_name, d_birth, d_death,country , cb) {
   authordetail = {first_name:first_name , family_name: family_name , country: country}
@@ -88,7 +92,21 @@ function genreCreate(name, cb) {
     cb(null, genre);
   }   );
 }
+////////////////////////////////////////////////////ALEX///////////////////////
+function formatoCreate(name, cb) {
+  var formato = new Formato({ name: name });
 
+  formato.save(function (err) {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    console.log('New Formato: ' + formato);
+    formatos.push(formato)
+    cb(null, formato);
+  }   );
+}
+//////////////////////////////////////////////////////////////////////////////////////
 function bookCreate(title, summary, isbn, author, genre, cb) {
   bookdetail = {
     title: title,
@@ -178,26 +196,41 @@ function createGenreAuthorsCountries(cb) {
 
 function createBooks(cb) {
     async.parallel([
+      ///////ALEX///////////////////////
+      function(callback) {
+        formatoCreate("CD", callback);
+      },
+      function(callback) {
+        formatoCreate("DVD", callback);
+      },
+      function(callback) {
+        formatoCreate("E-BOOK", callback);
+      },
+      function(callback) {
+        formatoCreate("i-tonto", callback);
+      },
+
+      //////////////////////////////////
         function(callback) {
-          bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],], callback);
+          bookCreate('The Name of the Wind (The Kingkiller Chronicle, #1)', 'I have stolen princesses back from sleeping barrow kings. I burned down the town of Trebon. I have spent the night with Felurian and left with both my sanity and my life. I was expelled from the University at a younger age than most people are allowed in. I tread paths by moonlight that others fear to speak of during day. I have talked to Gods, loved women, and written songs that make the minstrels weep.', '9781473211896', authors[0], [genres[0],],[formatos[0],formatos[1]], callback);
         },
         function(callback) {
-          bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],], callback);
+          bookCreate("The Wise Man's Fear (The Kingkiller Chronicle, #2)", 'Picking up the tale of Kvothe Kingkiller once again, we follow him into exile, into political intrigue, courtship, adventure, love and magic... and further along the path that has turned Kvothe, the mightiest magician of his age, a legend in his own time, into Kote, the unassuming pub landlord.', '9788401352836', authors[0], [genres[0],],[formatos[1],formatos[2]], callback);
         },
         function(callback) {
-          bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],], callback);
+          bookCreate("The Slow Regard of Silent Things (Kingkiller Chronicle)", 'Deep below the University, there is a dark place. Few people know of it: a broken web of ancient passageways and abandoned rooms. A young woman lives there, tucked among the sprawling tunnels of the Underthing, snug in the heart of this forgotten place.', '9780756411336', authors[0], [genres[0],],[formatos[0],], callback);
         },
         function(callback) {
-          bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],], callback);
+          bookCreate("Apes and Angels", "Humankind headed out to the stars not for conquest, nor exploration, nor even for curiosity. Humans went to the stars in a desperate crusade to save intelligent life wherever they found it. A wave of death is spreading through the Milky Way galaxy, an expanding sphere of lethal gamma ...", '9780765379528', authors[1], [genres[1],],[formatos[2],formatos[3]], callback);
         },
         function(callback) {
-          bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],], callback);
+          bookCreate("Death Wave","In Ben Bova's previous novel New Earth, Jordan Kell led the first human mission beyond the solar system. They discovered the ruins of an ancient alien civilization. But one alien AI survived, and it revealed to Jordan Kell that an explosion in the black hole at the heart of the Milky Way galaxy has created a wave of deadly radiation, expanding out from the core toward Earth. Unless the human race acts to save itself, all life on Earth will be wiped out...", '9780765379504', authors[1], [genres[1],],[formatos[1],formatos[3]], callback);
         },
         function(callback) {
-          bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]], callback);
+          bookCreate('Test Book 1', 'Summary of test book 1', 'ISBN111111', authors[4], [genres[0],genres[1]],[formatos[2],formatos[3]], callback);
         },
         function(callback) {
-          bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false, callback)
+          bookCreate('Test Book 2', 'Summary of test book 2', 'ISBN222222', authors[4], false,false, callback)
         }
         ],
         // optional callback
